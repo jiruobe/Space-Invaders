6d41b68ede3223a9f41357ff95d9df5bd597c622
jiruobe <95926915+jiruobe@users.noreply.github.com>
2022-01-26 20:51:33 +0000

    Update original.js

--- Space Invaders/original.js
+++ Space Invaders/original.js
@@ -1,11 +1,13 @@
-const KEY_RIGHT = 39;
-const KEY_LEFT = 37;
-const KEY_SPACE = 32;
+const KEY_RIGHT = 39;//Assigning value to the 'right arrow' key 
+const KEY_LEFT = 37;//Assigning value to the 'left arrow' key
+const KEY_SPACE = 32; //Assigning value to the 'space' key
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+const GAME_WIDTH = 800; // Making the game width 800px
+const GAME_HEIGHT = 600; //Making the game height 600px 
 
+//Creating a dictionary called STATE that holds all the properties and their inital values when the game starts
 const STATE = {
+    score: 0,  
     x_pos : 0,
     y_pos : 0,
     move_right: false,
@@ -22,6 +24,11 @@ const STATE = {
     gameOver: false 
 }
 
+// Initialize the Game
+const $container = document.querySelector(".main");
+createPlayer($container);
+createEnemies($container);
+
 // General purpose functions
 function setPosition($element, x, y) {
     $element.style.transform = `translate(${x}px, ${y}px)`;
@@ -32,14 +39,14 @@ function setSize($element, width) {
     $element.style.height = "auto";
 }
 
+// Deletes lasers that touch an enemy as well as deleting lasers that go off the game window 
 function deleteLaser(lasers, laser, $laser){
     const index = lasers.indexOf(laser);
     lasers.splice(index,1);
     $container.removeChild($laser);
 }
 
-//Restricts rocket's movements to the canvas
-
+//Restricts rocket's movements to the game window
 function bound(x){
     if (x >= GAME_WIDTH-STATE.spaceship_width){
         STATE.x_pos = GAME_WIDTH-STATE.spaceship_width;
@@ -52,7 +59,7 @@ function bound(x){
     }
 }
 
-// Player
+// Creating the player's spaceship 
 function createPlayer($container) {
     STATE.x_pos = GAME_WIDTH / 2;
     STATE.y_pos = GAME_HEIGHT - 50;
@@ -64,6 +71,7 @@ function createPlayer($container) {
     setSize($player, STATE.spaceship_width);
 }
 
+// For movement of the player's spaceship
 function updatePlayer(){
     if(STATE.move_left){
         STATE.x_pos -= 3;
@@ -93,34 +101,7 @@ function createLaser($container, x, y){
     setPosition($laser, x, y);
 }
 
-function updateLaser($container){
-    const lasers = STATE.lasers;
-    for(let i = 0; i < lasers.length; i++){
-        const laser = lasers[i];
-        laser.y -= 2;
-        if (laser.y < 0){
-            deleteLaser(lasers, laser, laser.$laser);
-        }
-        setPosition(laser.$laser, laser.x, laser.y)
-        const laser_rectangle = laser.$laser.getBoundingClientRect();
-        const enemies = STATE.enemies;
-        for(let j = 0; j < enemies.length; j++){
-        const enemy = enemies[j];
-        const enemy_rectangle = enemy.$enemy.getBoundingClientRect();
-        if(collideRect(enemy_rectangle, laser_rectangle)){
-            deleteLaser(lasers, laser, laser.$laser);
-            const index = enemies.indexOf(enemy);
-            enemies.splice(index,1);
-            $container.removeChild(enemy.$enemy);
-        }
-    
-      }
-    }
-}
-
-
-
-//Checks if any lasers touch an enemy 
+//This will be used to check if a player's laser has touched an enemy or if an enemy laser touches the player
 function collideRect(rect1, rect2){
     return!(rect2.left > rect1.right || 
       rect2.right < rect1.left || 
@@ -128,7 +109,36 @@ function collideRect(rect1, rect2){
       rect2.bottom < rect1.top);
 }
 
-// Enemy 
+//To make the laser move
+function updateLaser($container){
+    const ADD_TO_SCORE = 20;  
+    const lasers = STATE.lasers;
+    for(let i = 0; i < lasers.length; i++){
+          const laser = lasers[i];
+          laser.y -= 2;
+        // removes player's lasers that reach the top of the game window   
+        if (laser.y < 0){
+          deleteLaser(lasers, laser, laser.$laser); 
+        }
+        setPosition(laser.$laser, laser.x, laser.y)
+        const laser_rectangle = laser.$laser.getBoundingClientRect();
+        const enemies = STATE.enemies;
+        for(let j = 0; j < enemies.length; j++){
+          const enemy = enemies[j];
+          const enemy_rectangle = enemy.$enemy.getBoundingClientRect();
+          if(collideRect(enemy_rectangle, laser_rectangle)){ //checks to see if player's laser has hit an enemy
+            STATE.score += ADD_TO_SCORE; // Incrementing the score by 20 each time an enemy is hit 
+            document.getElementById("score").innerHTML = STATE.score;         
+            deleteLaser(lasers, laser, laser.$laser); // Deletes lasers that hit enemies 
+            const index = enemies.indexOf(enemy);
+            enemies.splice(index,1);
+            $container.removeChild(enemy.$enemy); // Removing the enemy that got hit 
+        }
+      }     
+    }
+}
+
+// To display the image for the enemy on the screen
 function createEnemy($container, x, y){
     const $enemy = document.createElement("img");
     $enemy.src = "ufo.png";
@@ -140,7 +150,8 @@ function createEnemy($container, x, y){
     setSize($enemy, STATE.enemy_width);
     setPosition($enemy, x, y);
 }
-  
+
+//For movement of the enemies 
 function updateEnemies($container){
     const dx = Math.sin(Date.now()/1000)*40;
     const dy = Math.cos(Date.now()/1000)*30;
@@ -157,7 +168,8 @@ function updateEnemies($container){
         enemy.enemy_cooldown -= 0.5;
     }
 }
-
+ 
+  
 // For creating muitliple rows of enemies
 function createEnemies($container) {
     for(var i = 0; i <= STATE.number_of_enemies/2; i++){
@@ -167,7 +179,7 @@ function createEnemies($container) {
     }
 }
 
-// Enemy Laser
+// Displaying the image for the enemy on the screen 
 function createEnemyLaser($container, x, y){
     const $enemyLaser = document.createElement("img");
     $enemyLaser.src = "enemyLaser.png";
@@ -177,12 +189,14 @@ function createEnemyLaser($container, x, y){
     STATE.enemyLasers.push(enemyLaser);
     setPosition($enemyLaser, x, y);
 }
-  
+
+// For the movement of the enemy laser 
 function updateEnemyLaser($container){
     const enemyLasers = STATE.enemyLasers;
     for(let i = 0; i < enemyLasers.length; i++){
         const enemyLaser = enemyLasers[i];
         enemyLaser.y += 2;
+        // removes enemy lasers that reach the bottom of the game window
         if (enemyLaser.y > GAME_HEIGHT-30){
             deleteLaser(enemyLasers, enemyLaser, enemyLaser.$enemyLaser);
         }
@@ -197,7 +211,7 @@ function updateEnemyLaser($container){
 
 
 
-// Key Presses / Controls
+// Enabling player's spaceship to move in the corresponding direction to the key pressed
 function KeyPress(event) {
     if(event.keyCode === KEY_RIGHT){
         STATE.move_right = true;
@@ -208,6 +222,7 @@ function KeyPress(event) {
     }
 }
 
+// Halts these movements when the keys are released
 function KeyRelease(event) {
     if (event.keyCode === KEY_RIGHT) {
         STATE.move_right = false;
@@ -226,7 +241,7 @@ function update(){
     updateEnemyLaser();
 
     window.requestAnimationFrame(update);
-
+    // Displays a win/lose screen depending on whether the player has won/lost their game 
     if (STATE.gameOver) {
         document.querySelector(".lose").style.display = "block";
     }   if (STATE.enemies.length == 0) {
@@ -234,13 +249,10 @@ function update(){
     }
 }
 
-// Initialize the Game
-const $container = document.querySelector(".main");
-createPlayer($container);
-createEnemies($container);
 
 // Key Press Event Listener
 window.addEventListener("keydown", KeyPress);
 window.addEventListener("keyup", KeyRelease);
 
+// calls the update function to enable movements of all the characters and their lasers 
 update();
